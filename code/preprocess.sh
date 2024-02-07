ebf210767779dd2ed0c91c6187a3439aeebe5ad6
DVSneuro <david.v.smith@temple.edu>
2024-02-06 20:52:10 -0500

    refactor code and split into submission script

---
+++ code/preprocess.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+maindir="$(dirname "$scriptdir")"
+rf1datadir=/ZPOOL/data/projects/rf1-sra-data
+
+sub=$1
+
+# make output directory
+mainoutput=${maindir}/derivatives/truenet-preprocess/sub-${sub}
+mkdir -p $mainoutput
+
+# ready inputs and outputs
+FLAIR=$rf1datadir/bids/sub-${sub}/anat/sub-${sub}_FLAIR.nii.gz
+T1=$rf1datadir/bids/sub-${sub}/anat/sub-${sub}_T1w.nii.gz
+outbase=$mainoutput/sub-${sub}
+
+# preprocess data only for existing data
+if [ -e $T1 ]; then
+	prepare_truenet_data --FLAIR=$FLAIR --T1=$T1 --outname=$outbase
+fi
