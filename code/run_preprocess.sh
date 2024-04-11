c257326f4ec2bcd1bdf248b04fbce50d5057949e
David V. Smith <david.v.smith@temple.edu>
2024-04-10 22:40:35 -0400

    run new subs

--- code/run_preprocess.sh
+++ code/run_preprocess.sh
@@ -6,7 +6,7 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
-for sub in `cat $scriptdir/sublist_all.txt`; do
+for sub in `cat $scriptdir/sublist-new.txt`; do
 
 	SCRIPTNAME=${scriptdir}/preprocess.sh
 	NCORES=25
