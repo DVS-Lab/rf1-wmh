95875c7f497e1c11c40365535ccb5d04884649de
David V. Smith <david.v.smith@temple.edu>
2024-04-11 22:02:16 -0400

    only run on existing data

--- code/flirt.sh
+++ code/flirt.sh
@@ -15,18 +15,22 @@ mkdir -p $mainoutput
 outbase=$mainoutput/Subject${sub}_T1_BrainAligned
 inputdatadir=/ZPOOL/data/projects/rf1-sra-data/derivatives/fmriprep
 
-# skull strip T1w
-fslmaths $inputdatadir/sub-${sub}/anat/sub-${sub}_desc-preproc_T1w.nii.gz \
--mas $inputdatadir/sub-${sub}/anat/sub-${sub}_desc-brain_mask.nii.gz \
-$mainoutput/sub-${sub}_masked
-
-# affine registration of skull stripped T1w
-flirt -in $mainoutput/sub-${sub}_masked \
--ref ${maindir}/masks/MNI152_T1_1mm_brain_LPS_filled.nii.gz \
--out $outbase \
--omat ${outbase}.mat \
--bins 256 -cost corratio -searchrx -90 90 -searchry -90 90 -searchrz -90 90 -dof 12  -interp trilinear
-
-# clean up tmp files
-rm -rf ${outbase}.mat
+if [ -e $inputdatadir/sub-${sub}/anat/sub-${sub}_desc-preproc_T1w.nii.gz ]; then
+	# skull strip T1w
+	fslmaths $inputdatadir/sub-${sub}/anat/sub-${sub}_desc-preproc_T1w.nii.gz \
+	-mas $inputdatadir/sub-${sub}/anat/sub-${sub}_desc-brain_mask.nii.gz \
+	$mainoutput/sub-${sub}_masked
+	
+	# affine registration of skull stripped T1w
+	flirt -in $mainoutput/sub-${sub}_masked \
+	-ref ${maindir}/masks/MNI152_T1_1mm_brain_LPS_filled.nii.gz \
+	-out $outbase \
+	-omat ${outbase}.mat \
+	-bins 256 -cost corratio -searchrx -90 90 -searchry -90 90 -searchrz -90 90 -dof 12  -interp trilinear
+	
+	# clean up tmp files
+	rm -rf ${outbase}.mat $mainoutput/sub-${sub}_masked.nii.gz
+	
+	echo "done with ${outbase}"
+fi
 
