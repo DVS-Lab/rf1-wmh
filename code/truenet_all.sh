1c755196ef9df3c78645b836bae5a18d13c20ce8
David V. Smith <david.v.smith@temple.edu>
2025-12-02 00:23:22 -0500

    add pretrained models

--- code/truenet_all.sh
+++ code/truenet_all.sh
@@ -4,6 +4,9 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
+# need to ensure it can find these since my install was bad
+export TRUENET_PRETRAINED_MODEL_PATH=$CONDA_PREFIX/data/truenet/models/
+
 # base directory for rf1 project
 rf1datadir=/ZPOOL/data/projects/rf1-sra-linux2
 
