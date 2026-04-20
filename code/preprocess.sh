0adcb2e5925616861b1ec64f2061a78116f845d1
David V. Smith <david.v.smith@temple.edu>
2026-04-20 19:55:43 -0400

    add if statement for missing T1w scans

--- code/preprocess.sh
+++ code/preprocess.sh
@@ -16,10 +16,18 @@ mkdir -p "$mainoutput"
 
 # ready inputs and outputs
 FLAIR=${rf1datadir}/bids/sub-${sub}/ses-${ses}/anat/sub-${sub}_ses-${ses}_FLAIR.nii.gz
-T1=${rf1datadir}/bids/sub-${sub}/ses-${ses}/anat/sub-${sub}_ses-${ses}_T1w.nii.gz
+if [[] $sub -eq 10590 || $sub -eq 10617 ]]; then # missing ses-2 T1w scans for these two for some reason
+    T1=${rf1datadir}/bids/sub-${sub}/ses-1/anat/sub-${sub}_ses-1_T1w.nii.gz
+else
+    T1=${rf1datadir}/bids/sub-${sub}/ses-${ses}/anat/sub-${sub}_ses-${ses}_T1w.nii.gz
+fi
 outbase=${mainoutput}/sub-${sub}
 
 # preprocess data only for existing data
 if [ -e "$T1" ] && [ -e "$FLAIR" ]; then
     prepare_truenet_data --FLAIR="$FLAIR" --T1="$T1" --outname="$outbase"
 fi
+
+
+ERROR: Could not find image /ZPOOL/data/projects/rf1-sra-linux2/bids/sub-10590/ses-02/anat/sub-10590_ses-02_T1w.nii.gz.nii.gz
+ERROR: Could not find image /ZPOOL/data/projects/rf1-sra-linux2/bids/sub-10617/ses-02/anat/sub-10617_ses-02_T1w.nii.gz.nii.gz
