3385444a422a3af4649d53f671d3ff98d4dea77a
David V. Smith <david.v.smith@temple.edu>
2026-04-20 20:15:02 -0400

    add zeropadding for ses

--- code/preprocess.sh
+++ code/preprocess.sh
@@ -8,7 +8,7 @@ maindir="$(dirname "$scriptdir")"
 rf1datadir=/ZPOOL/data/projects/rf1-sra-linux2
 
 sub=$1
-ses=02  # hard-code session for now
+ses=02  # hard-code session for now; need to stop zero-padding these
 
 # make output directory
 mainoutput=${maindir}/derivatives/truenet-preprocess/sub-${sub}/ses-${ses}
@@ -16,9 +16,9 @@ mkdir -p "$mainoutput"
 
 # ready inputs and outputs
 FLAIR=${rf1datadir}/bids/sub-${sub}/ses-${ses}/anat/sub-${sub}_ses-${ses}_FLAIR.nii.gz
-if [[ $sub -eq 10590 || $sub -eq 10617 ]]; then # missing ses-2 T1w scans for these two for some reason
-    T1=${rf1datadir}/bids/sub-${sub}/ses-1/anat/sub-${sub}_ses-1_T1w.nii.gz
-    echo "using ses-1 T1w for sub-${sub}..."
+if [[ $sub -eq 10590 || $sub -eq 10617 ]]; then # missing ses-02 T1w scans for these two for some reason
+    T1=${rf1datadir}/bids/sub-${sub}/ses-01/anat/sub-${sub}_ses-01_T1w.nii.gz
+    echo "using ses-01 T1w for sub-${sub}..."
 else
     T1=${rf1datadir}/bids/sub-${sub}/ses-${ses}/anat/sub-${sub}_ses-${ses}_T1w.nii.gz
 fi
